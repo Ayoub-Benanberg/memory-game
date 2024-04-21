23f9f6ff1d7ffab46b299103302ed5a2ab6b89e1
Ayoub-Benanberg <ayoubvufci@gmail.com>
2024-04-21 13:56:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const container = document.createElement("div");
+
 const select = document.createElement("select");
 select.setAttribute("id", "selected-Format");
 select.style.marginRight = "8px";
+
+
+//get the selected value to know how many blocks i need 
+select.addEventListener("change" , function(){
+  const selectedValue = select.value;
+  console.log(selectedValue)
+
+  // block.innerText = selectedValue
+})
+
 
 const options = ["Select format", "3x2", "4x2", "4x3", "4x4"];
 options.forEach(function(optionText) {
   const option = document.createElement("option");
   option.text = optionText;
   select.appendChild(option);
+
 });
 
 
+
+// the block container that gonna containe the blocks
 const blocksContainer = document.createElement("div");
 blocksContainer.setAttribute("class","block-container")
 blocksContainer.style.display = "flex";
@@ -27,12 +41,14 @@ rows.style.display = "flex";
 rows.appendChild(block);
 
 
-const generatebtn = document.createElement("button");
-generatebtn.innerText = "Generate";
+const generatbtn = document.createElement("button");
+generatbtn.innerText = "Generate";
 
 
 document.body.appendChild(container);
 container.appendChild(select);
-container.appendChild(generatebtn);
+container.appendChild(generatbtn);
 container.appendChild(blocksContainer);
 blocksContainer.appendChild(rows);
+
+
