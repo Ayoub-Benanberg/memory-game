6f0ca18cf46a6690cde27310792accf268407d27
Ayoub-Benanberg <ayoubvufci@gmail.com>
2024-04-21 01:10:03 +0100

    fixing problems

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ const select = document.createElement("select");
 select.setAttribute("id", "selected-Format");
 select.style.marginRight = "8px";
 
-
 const options = ["Select format", "3x2", "4x2", "4x3", "4x4"];
-options.forEach(function (optionText) {
+options.forEach(function(optionText) {
   const option = document.createElement("option");
   option.text = optionText;
   select.appendChild(option);
@@ -13,29 +12,27 @@ options.forEach(function (optionText) {
 
 
 const blocksContainer = document.createElement("div");
+blocksContainer.setAttribute("class","block-container")
 blocksContainer.style.display = "flex";
 blocksContainer.style.flexDirection = "column";
 
 
+
+const block = document.createElement("div");
+block.setAttribute("class","block")
+
 const rows = document.createElement("div");
+rows.setAttribute("class","rows");
 rows.style.display = "flex";
 rows.appendChild(block);
 
 
-const block = document.createElement("div");
-
-
-
-
-
-
-
-const generatbtn = document.createElement("button");
-generatbtn.innerText = "Generate";
+const generatebtn = document.createElement("button");
+generatebtn.innerText = "Generate";
 
 
 document.body.appendChild(container);
 container.appendChild(select);
-container.appendChild(generatbtn);
-container.appendChild(blocksContainer)
-blocksContainer.appendChild(rows)
+container.appendChild(generatebtn);
+container.appendChild(blocksContainer);
+blocksContainer.appendChild(rows);
